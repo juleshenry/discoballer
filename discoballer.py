2b977d81d4580dd8eeb8a0a44763c9c1763bcb72
Emmanuel Enri <emmanuelenri@Emmanuels-MBP.attlocal.net>
2024-08-03 18:11:06 -0500

    é legal

--- discoballer.py
+++ discoballer.py
@@ -1,28 +1,13 @@
 import subprocess
 import threading
 import queue
-
-def get_playlist_urls(playlist_url):
-    ydl_opts = {
-        'quiet': True,
-        'extract_flat': True,
-        'force_generic_extractor': True,
-        'forceurl': True,
-        'simulate': True,
-    }
-
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        result = ydl.extract_info(playlist_url, download=False)
-        if 'entries' in result:
-            return [f"https://www.youtube.com/watch?v={entry['url']}" for entry in result['entries']]
-
 MAX_CONCURRENT_CALLS = 8
 
 def system_call(command):
     subprocess.call(command, shell=True)
 
 def worker(queue):
-    while True:
+    while 1:
         command = queue.get()
         if command is None:
             break
@@ -32,26 +17,19 @@ def worker(queue):
 def make_system_calls(commands):
     qu = queue.Queue()
     threads = []
-
     for _ in range(MAX_CONCURRENT_CALLS):
         thread = threading.Thread(target=worker, args=(qu,))
         thread.start()
         threads.append(thread)
-
     for command in commands:
         qu.put(command)
-
     # Wait for all commands to finish
     qu.join()
-
     # Stop worker threads
     for _ in range(MAX_CONCURRENT_CALLS):
         qu.put(None)
-
     for thread in threads:
         thread.join()
-
-
 
 def get_urls_from_txt(file):
     with open(file) as s:
@@ -64,7 +42,6 @@ def get_urls_from_playlist_url(playlist_url):
     May have to run this:
     '/Applications/Python 3.12/Install\ Certificates.command 
     """
-
     playlist = Playlist(playlist_url)
     return playlist.video_urls
 
@@ -72,7 +49,7 @@ if __name__=='__main__':
     # Example usage
     commands = []
     # get_urls_from_txt('urls.txt')
-    playlist_url = 'https://youtube.com/playlist?list=PLVahv1rzEuwXMIxfsF2hftWYdyKdt-ukz&feature=shared'
+    playlist_url = ''
     urls = get_urls_from_playlist_url(playlist_url)
     print(f'got { len(urls)} urls',)
     commands = [f"yt-dlp -x --ignore-errors --audio-format 'mp3' {x}" for x in urls]
